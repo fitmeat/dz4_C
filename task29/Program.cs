b5bd8a236e49e28b5c71d3ab343b837b24492064
fitmeat <eenot825@gmail.com>
2022-12-02 15:26:42 +0300

    Выполнил задание 29

---
+++ task29/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+int[] arr = new int[8];
+for(int i =0; i<arr.Length;i++){
+    Console.WriteLine($"Введите {i+1} элемент массива: ");
+    arr[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine($"[{string.Join(", ", arr)}]");
