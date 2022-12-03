29910b024d5e9d77ab3ed289c3da3f8f04af5467
fitmeat <eenot825@gmail.com>
2022-12-03 18:20:33 +0300

    Выполнил задание 27

---
+++ task27/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+string? n = Console.ReadLine();
+int sum = 0;
+for(int i=0; i<=n.Length-1;i++){
+    sum = sum + int.Parse(n[i].ToString());
+}
+Console.WriteLine(sum);
